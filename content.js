5336be932acd84ff757778c88e6e759ab2a3ea0e
rdwxth <142283066+rdwxth@users.noreply.github.com>
2023-09-04 19:04:03 +0000

    Commits

--- content.js
+++ content.js
@@ -1,55 +1,12 @@
+// content.js
+
+// Listen for messages from the background script or other parts of your extension
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.text && message.fromKeybind) {
-      // Retrieve user settings from Chrome storage
-      chrome.storage.sync.get(['rayId'], function (data) {
-        const rayId = data.rayId;
-  
-        if (!rayId) {
-          alert('Please set your Ray ID in the extension settings.');
-          return;
-        }
-  
-        // Send the selected text to your API with the user's ray_id and handle the response here.
-        const selectedText = message.text;
-  
-        fetch('https://prod-1-eu-germany.trykai.xyz/api/chat', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${rayId}` // Use the user's ray_id as the API key
-          },
-          body: JSON.stringify({
-            ray_id: rayId,
-            message: selectedText
-          })
-        })
-        .then(response => response.json())
-        .then(data => {
-          const reply = JSON.parse(data.response).reply;
-          
-          // Copy the reply to the clipboard
-          const tempInput = document.createElement('input');
-          tempInput.value = reply;
-          document.body.appendChild(tempInput);
-          tempInput.select();
-          document.execCommand('copy');
-          document.body.removeChild(tempInput);
-          
-          // Notify the user that the response has been copied
-          alert('Response copied to clipboard: ' + reply);
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
-      });
-    }
-  
-    if (message.contextMenuClick) {
-      // Handle context menu click
+    if (message.text) {
       // Send the selected text to your API with the user's ray_id and handle the response here.
       const selectedText = message.text;
   
-      // Retrieve user settings from Chrome storage
+      // Retrieve user settings (Ray ID) from Chrome storage
       chrome.storage.sync.get(['rayId'], function (data) {
         const rayId = data.rayId;
   
@@ -58,7 +15,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
           return;
         }
   
-        // Send the selected text to your API with the user's ray_id and handle the response here.
+        // Send the selected text to your API
         fetch('https://prod-1-eu-germany.trykai.xyz/api/chat', {
           method: 'POST',
           headers: {
@@ -90,5 +47,6 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
         });
       });
     }
+    // Handle other message types as needed
   });
   
